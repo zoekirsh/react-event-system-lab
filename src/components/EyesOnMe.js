d54081d331e706d539b7a2b9a4c211e4db684857
Zoe Kirsh <zoekirsh@gmail.com>
2021-02-03 12:05:24 -0800

    green wall, baby

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  render() {
+    return (
+      <button
+        onBlur={() => {console.log("Hey! Eyes on me!")}}
+        onFocus={() => {console.log("Good!")}}>
+      Focus here
+      </button>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  render(){
+    return (
+      <input
+        onKeyUp={() => {console.log('Entering password...')}}
+        type="password">
+      </input>
+    )
+  }
+}
+
+export default Keypad
